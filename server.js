b8511cd0050f50a52d9abd3730eec830c2d32528
Eric Scott <Thunderducky@users.noreply.github.com>
2020-07-28 11:00:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(passport.session());
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
-
+console.log("Syncing DB");
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
